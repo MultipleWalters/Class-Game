ef84eed384974083b6655666273e4ee727956de4
DerogeNaN <132528861+DerogeNaN@users.noreply.github.com>
2023-10-18 12:38:16 +1100

    Camera Mechanics
    Adjusted camera script to scale UI elements relative to in-game sizes.

--- Assets/Scripts/Hitbox.cs
+++ Assets/Scripts/Hitbox.cs
@@ -9,21 +9,42 @@ public class Hitbox : MonoBehaviour
     [SerializeField] private Camera gameplayCamera;
     private Canvas cv;
     private RectTransform canvasRect;
+    private Collider playerCollider;
+
+    public Vector3 hitboxMin;
+    public Vector3 hitboxMax;
 
     private void Start()
     {
         cv = hitbox.GetComponentInParent<Canvas>();
         canvasRect = cv.GetComponent<RectTransform>();
+
+        playerCollider = GetComponent<Collider>();
     }
 
     public void UpdateCollision()
     {
-        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(gameplayCamera, transform.position);
+        hitboxMin = playerCollider.bounds.min;
+        hitboxMin.z = transform.position.z;
+        hitboxMax = playerCollider.bounds.max;
+        hitboxMax.z = transform.position.z;
+
+        Vector2 screenSpaceMin = TranslateToScreen(hitboxMin);
+        Vector2 screenSpaceMax = TranslateToScreen(hitboxMax);
+        
+        hitbox.anchoredPosition = TranslateToScreen(transform.position);
+        hitbox.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, Mathf.Abs(screenSpaceMax.x - screenSpaceMin.x));
+        hitbox.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, Mathf.Abs(screenSpaceMax.y - screenSpaceMin.y));
+
+    }
+
+    public Vector2 TranslateToScreen(Vector3 worldPos)
+    {
+        Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(gameplayCamera, worldPos);
         Vector2 result;
         RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRect, screenPoint, cv.renderMode == RenderMode.ScreenSpaceOverlay ? null : gameplayCamera, out result);
 
+        return result;
+    }
 
-        hitbox.anchoredPosition = result;
-
-    }    
 }
